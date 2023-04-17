1be12fa7b035f79f25f869305ebbc6b524489aa1
Drew Chase <dcmanproductions@gmail.com>
2023-04-17 03:13:31 -0400

    Added theme field

--- common/src/main/java/chase/minecraft/architectury/fluidui/FluidUI.java
+++ common/src/main/java/chase/minecraft/architectury/fluidui/FluidUI.java
@@ -8,6 +8,7 @@ public class FluidUI
 {
 	public static final String MOD_ID = "fluidui";
 	public static final Logger log = LogManager.getLogger(MOD_ID);
+	public static final FluidTheme THEME = FluidTheme.DARK;
 	
 	public static void init()
 	{
