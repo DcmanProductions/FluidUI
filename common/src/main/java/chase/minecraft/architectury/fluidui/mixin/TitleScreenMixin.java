d7fd62815be8a3b5a18eb753b05499b497151739
Drew Chase <dcmanproductions@gmail.com>
2023-04-20 03:21:19 -0400

    added button to title screen in developer mode

--- common/src/main/java/chase/minecraft/architectury/fluidui/mixin/TitleScreenMixin.java
+++ common/src/main/java/chase/minecraft/architectury/fluidui/mixin/TitleScreenMixin.java
@@ -1,7 +1,9 @@
 package chase.minecraft.architectury.fluidui.mixin;
 
-import chase.minecraft.architectury.fluidui.TestScreen;
+import chase.minecraft.architectury.fluidui.FluidTheme;
+import chase.minecraft.architectury.fluidui.gui.component.FluidButtonWidget;
 import chase.minecraft.architectury.fluidui.impl.AsyncThemeLoader;
+import dev.architectury.platform.Platform;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.TitleScreen;
 import net.minecraft.network.chat.Component;
@@ -9,6 +11,9 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import test.TestScreen;
+
+import static dev.architectury.hooks.client.screen.ScreenHooks.addRenderableWidget;
 
 @Mixin(TitleScreen.class)
 public class TitleScreenMixin
@@ -16,11 +21,18 @@ public class TitleScreenMixin
 	@Inject(at = @At("RETURN"), method = "init")
 	void init(CallbackInfo cb)
 	{
-		TitleScreen screen = (TitleScreen) ((Object) this);
-		Minecraft minecraft = Minecraft.getInstance();
-		
+		// Loads all the themes
 		AsyncThemeLoader.getInstance().loadAll();
-		
-		minecraft.setScreen(new TestScreen(Component.literal("Test"), screen));
+
+//		 TODO: Remove after testing
+		if (Platform.isDevelopmentEnvironment())
+		{
+			TitleScreen screen = (TitleScreen) ((Object) this);
+			addRenderableWidget(screen,
+					new FluidButtonWidget(FluidTheme.DEFUALT, Component.literal("Show Demo"), 5, 5, 100, 20, b ->
+					{
+						Minecraft.getInstance().setScreen(new TestScreen(Component.literal("Test"), screen));
+					}));
+		}
 	}
 }
